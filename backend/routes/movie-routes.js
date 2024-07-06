548d882813a511648556a420711ec35013917202
Sachin Yadav <sachinydv755@gmail.com>
2024-07-06 18:15:39 +0530

    Restore folder_name

--- backend/routes/movie-routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addMovie } from "../controllers/movie-controller";
-const movieRouter = express.Router();
-// movieRouter.get("/", getAllMovies);
-// movieRouter.get("/:id", getMovieById);
-movieRouter.post("/", addMovie);
-
-export default movieRouter;
